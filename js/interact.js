e3637d06d7b26c76e306acf08e4f27ceb0cf4e7b
Edgar Lopez <edgarylop@gmail.com>
2016-12-21 16:19:51 -0800

    Fixed UI elements.

---
+++ js/interact.js
@@ -0,0 +1,25 @@
+(() =>{
+
+    var doggies = [{
+            name: "Spike",
+            age: "15",
+            img: "img/handsome-dog.jpg"
+        },{
+            name: "Miski",
+            age: "10",
+            img: "img/miski.jpg"
+        },{
+            name: "Doug",
+            age: "7",
+            img: "img/doug.jpg"
+    }];
+
+    $("document").ready(() => {
+        // Populate
+        
+    });
+
+    console.log(doggies);
+})();
+
+
