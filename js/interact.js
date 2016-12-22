7d1b2c224d9ec05c8b73942ed3bc45f6d659d441
Edgar Lopez <edgarylop@gmail.com>
2016-12-21 19:19:48 -0800

    Portfolios now added based on JS code.

--- js/interact.js
+++ js/interact.js
@@ -3,19 +3,42 @@
     var doggies = [{
             name: "Spike",
             age: "15",
-            img: "img/handsome-dog.jpg"
+            img: "img/dog1.jpg"
         },{
             name: "Miski",
             age: "10",
-            img: "img/miski.jpg"
-        },{
-            name: "Doug",
-            age: "7",
-            img: "img/doug.jpg"
-    }];
+            img: "img/dog2.jpg"
+        }];
+
+        /*
+        <div class="portfolio-container">
+                <div class="portfolio">
+                    <img src="img/dog1.jpg" alt="Image of dog." class="img-responsive">
+                    <h4>Spike, 14</h4>
+                </div>
+            </div>*/
+
+    
 
     $("document").ready(() => {
         // Populate
+        var centerBlockPortfolio = $(".center-block-portfolio");
+        centerBlockPortfolio.append('<div class="portfolio-container"></div>');
+        var portfolioContainer = $(".portfolio-container");
+        portfolioContainer.append('<div class="portfolio"></div>');
+        var portfolio = $(".portfolio");
+        portfolio.append(`<img src=${doggies[0].img} alt="Image of ${doggies[0].name}" class="img-responsive">`);
+        portfolio.append(`<h4>${doggies[0].name}, ${doggies[0].age}</h4>`);
+
+        doggies.forEach( (element, index) =>{
+            var centerBlockPortfolio = $(".center-block-portfolio");
+            centerBlockPortfolio.append(`<div id=${index} class="portfolio-container-overlay"></div>`);
+            var portfolioContainer = $(`#${index}`);
+            portfolioContainer.append('<div class="portfolio"></div>');
+            var portfolio = $(`#${index} > .portfolio`);
+            portfolio.append(`<img src=${element.img} alt="Image of ${element.name}" class="img-responsive">`);
+            portfolio.append(`<h4>${element.name}, ${element.age}</h4>`);
+        } );
         
     });
 
